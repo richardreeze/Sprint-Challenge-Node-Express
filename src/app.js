2822f7c056f0da8d1c47c395795c8bc3b6833bc0
Richard Reis <richardreeze1@gmail.com>
2018-03-09 09:21:13 -0800

    Show today's price

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const fetch = require('node-fetch');
 
 const app = express();
 
 app.get('/compare', (req, res) => {
-  res.send('hello');
+  fetch('https://api.coindesk.com/v1/bpi/currentprice/BTC.json')
+    .then(data => data.json())
+    .then(currentPrice => console.log(currentPrice.bpi.USD.rate))
 });
 
 app.listen(3000);
